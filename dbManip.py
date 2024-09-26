7a1a38919c525f610bfb36e276b4beda68165afd
AlexLobter <wrightalex507@gmail.com>
2024-09-26 12:29:56 -0500

    Merge branch 'main' of https://github.com/RAPTERCAR/CS-396-2

--- dbManip.py
+++ dbManip.py
@@ -1,2 +1,14 @@
 import sqlite3
 import bcrypt
+
+def createQuiz(qN,tL):
+    conn = sqlite3.connect('quiz.db')
+    cursor = conn.cursor()
+    cursor.execute(
+       "INSERT OR IGNORE INTO quiz (qName, time) VALUES (?, ?)",
+       (qN,tL))
+    s = 'quiz added'
+    print("should work")
+    conn.commit()
+    conn.close()
+    return s
--- static/js/admin.js
+++ static/js/admin.js
@@ -2,23 +2,19 @@ document.getElementById('submitQ').addEventListener('click', function() {
     // Example AJAX call using fetch API
     document.getElementById('msgs').innerHTML = '';
     console.log('sending data');
-    fetch('/ajaxkeyvalue', {
+    fetch('/data', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
         },
-        body: JSON.stringify({ 'qName': document.getElementById('qName').value,
+        body: JSON.stringify({ 'request': "createQuiz",
+                               'qName': document.getElementById('qName').value,
                                "tLimit": document.getElementById('tLimit').value}),
     })
     .then(response => response.json())
     .then(data => {
-      if (data.status == 'ok') {
-          // Redirect to /library on success
-          window.location.href = 'home';
-      } else {
-        // Update the 'msgs' div with the error message
-         document.getElementById('msgs').innerHTML = '<p style= "color:red;">Error: ..........</p>';
-      }
+        console.log('test');
+        document.getElementById('output').innerHTML = data.output;
     })
     .catch(error => {
         console.error('Error:', error);
