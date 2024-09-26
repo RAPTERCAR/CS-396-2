bb04a8668e42427c2191cf4bda3b92352afab8c4
RAPTERCAR <rileywoodruff03@gmail.com>
2024-09-26 18:53:44 -0500

    minor stuff
    adafsdfadf

--- static/js/admin.js
+++ static/js/admin.js
@@ -1,22 +1,26 @@
-document.getElementById('submitQ').addEventListener('click', function() {
-    // Example AJAX call using fetch API
-    document.getElementById('msgs').innerHTML = '';
-    console.log('sending data');
-    fetch('/data', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ 'request': "createQuiz",
-                               'qName': document.getElementById('qName').value,
-                               "tLimit": document.getElementById('tLimit').value}),
-    })
-    .then(response => response.json())
-    .then(data => {
-        console.log('test');
-        document.getElementById('output').innerHTML = data.output;
-    })
-    .catch(error => {
-        console.error('Error:', error);
+window.onload=function(){    
+    
+    document.getElementById('submitQ').addEventListener('click', function() {
+        // Example AJAX call using fetch API
+        document.getElementById('msgs').innerHTML = '';
+        console.log('sending data');
+        fetch('/data', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ 'request': "createQuiz",
+                                'qName': document.getElementById('qName').value,
+                                "tLimit": document.getElementById('tLimit').value}),
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log('test');
+            document.getElementById('output').innerHTML = data.output;
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
     });
-});
+
+}
